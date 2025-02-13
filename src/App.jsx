3cf5918e5e2175a1ad5ea60de74f4c1684295fd3
enteaunain <enteaunain@gmail.com>
2025-02-13 15:53:17 +0100

    On défile et on peut attaquer

--- src/App.jsx
+++ src/App.jsx
@@ -4,27 +4,18 @@ import viteLogo from '/vite.svg'
 import CartePok from './components/cartePok/cartePok.jsx'
 import './App.css'
 
-//const bulbizarre = pokemons[0]
-
 function App() {
   var [count, setCount] = useState(0)
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
+      
+      <h1>POKEMON</h1>
       <h3>Appuyez sur le compteur pour faire défiler les pokémons !</h3>
-      <div className="card">
+      <grid className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           Le compteur est à {count}
         </button>
-      </div>
+      </grid>
       <CartePok count={count} />
     </>
   )
--- src/components/cartePok/cartePok.jsx
+++ src/components/cartePok/cartePok.jsx
@@ -1,28 +1,59 @@
 import pokemons from "../../assets/pokemons"
+import { useEffect, useState } from "react"
 
-const cartePok = ({count}) => {
-    console.log(count)
-    if (count === 0) {
+const CartePok = ({count}) => {
+
+    if (count === 0 || count >= 152){
         return (
             <div>
-                <p>Le compteur est à 0</p>
+                <p>Le compteur est à {count}</p>
             </div>
         )
     }
-    else if (count > 0 && count <= 152){
-        const pokemon = pokemons[count - 1]
+    else{
+        let pokemon = pokemons[count - 1]
         console.log(pokemon)
+
+        // ça c'est des states
+        let [currentHp, setCurrentHp] = useState(pokemon.base.HP)
+
+        // ensuite les useEffect
+        useEffect(() => {
+            if (currentHp <= 0){
+                setCurrentHp(0)
+                alert("Le pokémon est KO !")
+            }
+        }), [currentHp]
+
+        useEffect(() => {
+            alert('Le combat commence')
+            setCurrentHp(pokemon.base.HP);
+        }, [count])
+
+
         return (
-        <div className="card">
-            <img src={pokemon.image} style={{height:180}} alt={pokemon.name.french} />
-            <h2>{pokemon.name.french}</h2>
-            <p>ID: {pokemon.id}</p>
-            <p>Type majoritaire: {pokemon.type[0]}</p>
-            <p>Attaque: {pokemon.base.Attack}, <br/>Défense: {pokemon.base.Defense}, <br/>HP: {pokemon.base.HP}, <br/>Attaque Spé.: {pokemon.base["Sp. Attack"]}, <br/>Défense Spé.: {pokemon.base["Sp. Defense"]}, <br/>Vitesse: {pokemon.base.Speed}</p>
+        <div>
+            <div className="card">
+                <img src={pokemon.image} style={{height:180}} alt={pokemon.name.french} />
+                <h2>{pokemon.name.french}</h2>
+                <p>ID: {pokemon.id}</p>
+                <p>Type:{" "} 
+                {pokemon.type.map((type, index) => (
+                    <span key={index}>{type}{index < pokemon.type.length - 1 ? ', ' : ''}</span>
+                ))}
+                </p>
+                <p>Attaque: {pokemon.base.Attack}, <br/>Défense: {pokemon.base.Defense}, <br/>HP: {currentHp}, <br/>Attaque Spé.: {pokemon.base["Sp. Attack"]}, <br/>Défense Spé.: {pokemon.base["Sp. Defense"]}, <br/>Vitesse: {pokemon.base.Speed}</p>
+            </div>
+            <button onClick={() => {
+                setCurrentHp(currentHp - 10)
+                if (currentHp <= 0){
+                    alert("Le pokémon est KO !")
+                }
+            }}>Attaquer</button>
         </div>
     )
     }
     
 }
 
-export default cartePok
+export default CartePok
